9e49fc2df6bef394ef1581499bce78f6a0b852a5
Theo <theoparisdesigns@gmail.com>
2020-11-09 13:28:11 -0800

    migrate tslint to eslint and remove typings

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+	},
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+	],
+	rules: {
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+	},
+};
